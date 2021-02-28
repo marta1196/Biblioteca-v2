789bb6fbd4ed73c3597bb4d63d95108a3b074b73
marta <marta@DESKTOP-TMB7CRH.home>
2021-02-28 18:44:37 +0100

    Refactorización de la clase Controlador y extraemos su interfaz.

--- src/main/java/org/iesalandalus/programacion/biblioteca/mvc/controlador/Controlador.java
+++ src/main/java/org/iesalandalus/programacion/biblioteca/mvc/controlador/Controlador.java
@@ -6,19 +6,19 @@ import java.util.Map;
 
 import javax.naming.OperationNotSupportedException;
 
-import org.iesalandalus.programacion.biblioteca.mvc.modelo.Modelo;
+import org.iesalandalus.programacion.biblioteca.mvc.modelo.IModelo;
 import org.iesalandalus.programacion.biblioteca.mvc.modelo.dominio.Alumno;
 import org.iesalandalus.programacion.biblioteca.mvc.modelo.dominio.Curso;
 import org.iesalandalus.programacion.biblioteca.mvc.modelo.dominio.Libro;
 import org.iesalandalus.programacion.biblioteca.mvc.modelo.dominio.Prestamo;
-import org.iesalandalus.programacion.biblioteca.mvc.vista.Vista;
+import org.iesalandalus.programacion.biblioteca.mvc.vista.IVista;
 
-public class Controlador {
+public class Controlador implements IControlador {
 
-	private Modelo modelo;
-	private Vista vista;
+	private IModelo modelo;
+	private IVista vista;
 
-	public Controlador(Modelo modelo, Vista vista) {
+	public Controlador(IModelo modelo, IVista vista) {
 
 		if (modelo == null) {
 
@@ -36,96 +36,115 @@ public class Controlador {
 		this.vista.setControlador(this);
 	}
 
+	@Override
 	public void comenzar() {
 
 		vista.comenzar();
 	}
 
+	@Override
 	public void terminar() {
 
 		System.out.println("Adiós");
 	}
 
+	@Override
 	public void insertar(Alumno alumno) throws OperationNotSupportedException {
 
 		modelo.insertar(alumno);
 	}
 
+	@Override
 	public void insertar(Libro libro) throws OperationNotSupportedException {
 
 		modelo.insertar(libro);
 	}
 
+	@Override
 	public void prestar(Prestamo prestamo) throws OperationNotSupportedException {
 
 		modelo.prestar(prestamo);
 	}
 
+	@Override
 	public void devolver(Prestamo prestamo, LocalDate fechaDevolver) throws OperationNotSupportedException {
 
 		modelo.devolver(prestamo, fechaDevolver);
 	}
 
+	@Override
 	public Alumno buscar(Alumno alumno) {
 
 		return modelo.buscar(alumno);
 	}
 
+	@Override
 	public Libro buscar(Libro libro) {
 
 		return modelo.buscar(libro);
 	}
 
+	@Override
 	public Prestamo buscar(Prestamo prestamo) {
 
 		return modelo.buscar(prestamo);
 	}
 
+	@Override
 	public void borrar(Alumno alumno) throws OperationNotSupportedException {
 
 		modelo.borrar(alumno);
 	}
 
+	@Override
 	public void borrar(Libro libro) throws OperationNotSupportedException {
 
 		modelo.borrar(libro);
 	}
 
+	@Override
 	public void borrar(Prestamo prestamo) throws OperationNotSupportedException {
 
 		modelo.borrar(prestamo);
 	}
 
+	@Override
 	public List<Alumno> getAlumnos() {
 
 		return modelo.getAlumnos();
 	}
 
+	@Override
 	public List<Libro> getLibros() {
 
 		return modelo.getLibros();
 	}
 
+	@Override
 	public List<Prestamo> getPrestamos() {
 
 		return modelo.getPrestamos();
 	}
 
+	@Override
 	public List<Prestamo> getPrestamos(Alumno alumno) {
 
 		return modelo.getPrestamos(alumno);
 	}
 
+	@Override
 	public List<Prestamo> getPrestamos(Libro libro) {
 
 		return modelo.getPrestamos(libro);
 	}
 
+	@Override
 	public List<Prestamo> getPrestamos(LocalDate fecha) {
 
 		return modelo.getPrestamos(fecha);
 	}
 
+	@Override
 	public Map<Curso, Integer> getEstadisticaMensualPorCurso(LocalDate mes) {
 
 		return modelo.getEstadisticaMensualPorCurso(mes);
