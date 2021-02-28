8cb0de3802214e846fab955d7071234e25a7d66d
marta <marta@DESKTOP-TMB7CRH.home>
2021-02-28 18:47:13 +0100

    Modificamos la clase MainApp.

--- src/main/java/org/iesalandalus/programacion/biblioteca/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/biblioteca/MainApp.java
@@ -1,17 +1,23 @@
 package org.iesalandalus.programacion.biblioteca;
 
 import org.iesalandalus.programacion.biblioteca.mvc.controlador.Controlador;
+import org.iesalandalus.programacion.biblioteca.mvc.controlador.IControlador;
+import org.iesalandalus.programacion.biblioteca.mvc.modelo.FactoriaFuenteDatos;
+import org.iesalandalus.programacion.biblioteca.mvc.modelo.IModelo;
 import org.iesalandalus.programacion.biblioteca.mvc.modelo.Modelo;
-import org.iesalandalus.programacion.biblioteca.mvc.vista.Vista;
+import org.iesalandalus.programacion.biblioteca.mvc.modelo.negocio.memoria.FactoriaFuenteDatosMemoria;
+import org.iesalandalus.programacion.biblioteca.mvc.vista.FactoriaVista;
+import org.iesalandalus.programacion.biblioteca.mvc.vista.IVista;
+import org.iesalandalus.programacion.biblioteca.mvc.vista.texto.VistaTexto;
 
 public class MainApp {
 
 	public static void main(String[] args) {
 
-		Modelo modelo = new Modelo();
-		Vista vista = new Vista();
+		IModelo modelo = new Modelo(FactoriaFuenteDatos.MEMORIA.crear());
+		IVista vista = FactoriaVista.TEXTO.crear();
 
-		Controlador controlador = new Controlador(modelo, vista);
+		IControlador controlador = new Controlador(modelo, vista);
 		controlador.comenzar();
 	}
 }
